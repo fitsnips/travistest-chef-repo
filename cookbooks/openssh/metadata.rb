d38c6b1af8e087072760f6fb48694735aaa3200b
Joshua Miller <joshua.miller@fitsnips.net>
2014-05-15 23:50:31 -0700

    old format metadata

---
+++ cookbooks/openssh/metadata.rb
@@ -0,0 +1,8 @@
+name 		  "openssh"
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
+license           "Apache 2.0"
+description       "Installs openssh"
+version "1.3.5"
+
+
