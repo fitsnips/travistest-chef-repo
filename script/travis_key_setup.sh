9fe003ea0767c58204e4f0592a80007aac56f0f6
Joshua Miller <joshua.miller@fitsnips.net>
2014-06-06 09:57:50 -0700

    yes this will expose the key and I will gen a new one but need to figure this out

--- script/travis_key_setup.sh
+++ script/travis_key_setup.sh
@@ -1,7 +1,7 @@
 travis encrypt DIGITALOCEAN_SSH_KEY_IDS=[$DIGITALOCEAN_SSH_KEY_IDS] --add
 travis encrypt DIGITALOCEAN_CLIENT_ID=$DIGITALOCEAN_CLIENT_ID --add
 travis encrypt DIGITALOCEAN_API_KEY=$DIGITALOCEAN_API_KEY --add
-travis encrypt DIGITALOCEAN_SSH_KEY_PATH="~/.ssh/id_do.pem" --add
+travis encrypt DIGITALOCEAN_SSH_KEY_PATH="$DIGITALOCEAN_SSH_KEY_PATH" --add
 base64 ~/.ssh/travisci_cook_digitalocean.pem | awk '{
   j=0;
   for( i=1; i<length; i=i+90 ) {
